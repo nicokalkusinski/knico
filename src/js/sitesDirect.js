6e9358f734d768ffb9f4db6afa7e95eb27b20686
raitrow <raitrow@users.noreply.github.com>
2021-09-20 23:26:45 +0200

    redirect to dmance

---
+++ src/js/sitesDirect.js
@@ -0,0 +1,4 @@
+let sites = document.getElementsByClassName("example-item");
+sites[0].addEventListener("click", () => {
+    window.open("https://raitrow.github.io/knico/projects/DMance/web/index.html");
+})
