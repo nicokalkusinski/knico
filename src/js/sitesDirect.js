a904589a42cb26c8aace0ee654b974cf7429dba4
raitrow <raitrow@users.noreply.github.com>
2021-10-21 13:46:47 +0200

    keonti redirect

--- src/js/sitesDirect.js
+++ src/js/sitesDirect.js
@@ -2,7 +2,7 @@ let sites = document.getElementsByClassName("example-item");
 sites[0].addEventListener("click", () => {
     window.open("https://raitrow.github.io/knico/projects/DMance/web/index.html");
 })
-sites[2].addEventListener("click", () => {
+sites[1].addEventListener("click", () => {
     window.open("https://raitrow.github.io/knico/projects/Keonti/web/index.html");
 })
 console.log(sites);
